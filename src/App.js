c7df5d04b717ed10b385253fbc6aa4ade35c4790
Kristen Bartel <kristenbartel@gmail.com>
2022-04-22 11:24:36 -0500

    nav and links comps and base styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar';
+import NavBar from './components/Navbar';
+import Home from './components/Home';
 
 function App() {
   return (
-  
+    <div className="App"> 
+        <Navbar/>
+        <div className="content">
+        <Home></Home>
+        </div>
+    </div>
   );
 }
-
 export default App;
